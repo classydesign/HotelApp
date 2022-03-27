099305a06158da7fe94b2d4228baf5f47c8eee1a
classydesign <gulshanbarkud0@gmail.com>
2022-03-27 15:21:55 +0530

    git push -u origin main

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Card from "./Card";
 import './App.css';
 import Banner from "./Panel";
 import List from "./List";
-import News from "./News";
+
 
 function App() {  
   return (
     <div className="App">
      <Banner/>
-     <News/>
+     
      <List/>
      <Card />     
     </div>
